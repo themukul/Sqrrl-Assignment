9d660202936d9542650de02d06538bc6320c8ddb
themukul <me.mukul05@gmail.com>
2018-05-07 11:38:48 +0530

    sign out

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ViewProfile from './ViewProfile';
 import EditProfile from './EditProfile';
 import './Dashboard.css';
 import * as firebase from 'firebase';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 class Dashboard extends React.Component{
 
@@ -16,12 +15,15 @@ class Dashboard extends React.Component{
 			age: "",
 			gender: "",
 			editProfile: 0,
+			user: null,
 		};
 
-		this.handleEditProfile = this.handleEditProfile.bind(this);	
+		this.handleEditProfile = this.handleEditProfile.bind(this);
+		this.handleSignOut = this.handleSignOut.bind(this);	
 	}	
 	
 	componentDidMount() {
+		const user = firebase.auth().currentUser;
 		const rootRef = firebase.database().ref().child('root');
 		//console.log(rootRef);
 		const userRef = rootRef.child('user');
@@ -31,7 +33,8 @@ class Dashboard extends React.Component{
 				name: snap.child('name').val(),
 				email: snap.child('email').val(),
 				age: snap.child('age').val(),
-				gender: snap.child('gender').val(),						
+				gender: snap.child('gender').val(),
+				user						
 			});
 		});
 
@@ -48,16 +51,24 @@ class Dashboard extends React.Component{
 			currentState
 		);
 	}
+	
+	handleSignOut() {
+		const currentState = this.state;
+		currentState.user = null;
+		firebase.auth().signOut().then(this.setState({currentState}));
+	}
 
 	render() {
 		if(this.state.editProfile){
 			return <Redirect to='/editprofile' />
 		}
-		
+		if(!(this.state.user)){
+			return <div> You must login to continue. <Link to='/signin'> Log In </Link></div>		
+		}
 		return(
 			<div>
 				<nav className="nav-bar">					
-					<button className="btn waves-effect waves-light sign-out-btn right">
+					<button className="btn waves-effect waves-light sign-out-btn right" onClick={this.handleSignOut}>
 						Sign Out
      					</button>
 					
